3035a583d794c94945c1f818da91c6fd3c1b4c84
lethisa <lethisaputri@gmail.com>
2018-07-13 18:09:08 +0700

    sample

---
+++ cms/app/Http/Controllers/PostController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        return "post id ". $id;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return "This is method create";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "this is show method " .$id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function contact()
+    {
+        $people = ['Edwin', 'Jose', 'Peter', 'Marian'];
+        return view('contact', compact('people'));
+    }
+
+    public function showPost($id, $name, $password)
+    {
+        // return view('post')->with('id', $id);
+        return view('post', compact('id', 'name', 'password'));
+    }
+}
--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -14,3 +15,136 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// pass parameter
+// Route::get('/post/{id}/{name}', function ($id, $name) {
+//     return "This is post number ". $id . " $name";
+// });
+
+// naming routes + nickname
+// Route::get('/admin/posts/example', array('as'=>'admin.home', function () {
+//     $url = route('admin.home');
+//     return "This url is". $url;
+// }));
+
+// connect controller + route
+// Route::get('/post/{id}', 'PostController@index');
+
+// special route - CRUD
+// Route::resource('posts', 'PostController');
+
+// Route::get('/contact', 'PostController@contact');
+
+// Route::get('post/{id}/{name}/{password}', 'PostController@showPost');
+
+
+// Insert Database
+// Route::get('/insert', function () 
+//     {
+//         DB::insert('INSERT INTO posts (title, content) VALUES (?, ?)', ['PHP Title', 'PHP Content']);
+//     });
+
+// Read Database
+// Route::get('/read', function()
+//     {
+//         $results = DB::select('SELECT * FROM posts WHERE id = ?', [1]);
+
+//         foreach ($results as $post) {
+//             return $post->title;
+//         }
+
+//         return var_dump($results);
+//     });
+
+// Update Database
+// Route::get('/update', function ()
+//     {
+//         $updated = DB::update('UPDATE posts SET title = "PHP Title New" WHERE id = ?', [1]);
+//
+//         return $updated;
+//     });
+
+// Delete Data
+// Route::get('/delete', function () 
+//     {
+//         $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [1]);
+
+//         return $deleted;
+//     });
+
+// ELOQUENT - ORM
+// Route::get('/find', function ()
+//     {
+//         // $posts = Post::all();
+
+//         // foreach ($posts as $post) {
+//         //     return $post->title;
+//         // }
+
+//         $posts = Post::find(2);
+//         return $posts->title;
+//     });
+
+// Route::get('/find', function () 
+//     {
+//         $posts = Post::where('id', 2)->orderBy('id', 'desc')->take(1)->get();
+//         return $posts;
+//     });
+
+// Route::get('/find', function () 
+//     {
+//         // $posts = Post::findOrFail(1);
+//         // return $posts;
+
+//         $posts = Post::where('users_count', '<', 50)->firstOrFail();
+//         return $posts;
+//     });
+
+// Route::get('/basicInsert', function ()
+//     {
+//         $post = new Post;
+
+//         $post->title = 'New ORM Title';
+//         $post->content = 'Content for ORM';
+
+//         $post->save();
+//     });
+
+// Route::get('/basicInsert2', function ()
+//     {
+//         $post = Post::find(2);
+
+//         $post->title = 'New Title';
+//         $post->content = 'Content for 2';
+
+//         $post->save();
+//     });
+
+// Route::get('/create', function ()
+//     {
+//         Post::create(['title'=>'Created Title', 'content'=>'Created Content']);
+
+//     });
+
+// Route::get('/update', function ()
+//     {
+//         Post::where('id', 2)->where('is_admin', 0)->update(['title'=>'New PHP2', 'content'=>'New']);
+//     });
+
+// Route::get('/delete', function ()
+//     {
+//         $post = Post::find(2);
+//         $post->delete();
+//     });
+
+// Route::get('/delete2', function ()
+//     {
+//         Post::destroy([4, 5]);
+
+//         // Post::where('is_admin', 0)->delete();
+//     });
+
+Route::get('/softDelete', function ()
+    {
+        Post::find(1)->delete();
+    });
