dcd7c4e9f7b0179d98c7966888d3af263147e244
lethisa <lethisaputri@gmail.com>
2018-07-18 17:40:04 +0700

    OneToOne sample

--- OneToOne/app/Http/routes.php
+++ OneToOne/app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Address;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -14,3 +17,40 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insert', function()
+    {
+        $user = User::findOrFail(2);
+
+        // return $user;
+
+        $address = new Address(['name'=>'1234 Alaska NS']);
+
+        $user->address()->save($address);
+    });
+
+Route::get('/update', function()
+    {
+        $address = Address::where('user_id', 1)->first();
+        // $address = Address::where('user_id', '=', 1);
+        // $address = Address::whereUserId(1);
+
+        $address->name = "New Address";
+        $address->save();
+
+    });
+
+Route::get('/read', function()
+    {
+        $user = User::findOrFail(1);
+
+        echo $user->address->name;
+    });
+
+Route::get('/delete', function()
+    {
+        $user = User::findOrFail(1);
+        $user->address()->delete();
+
+        return "done";
+    });
--- OneToOne/app/User.php
+++ OneToOne/app/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function ()
+    public function address()
     {
         return $this->hasOne('App\Address');
     }
