41e1d65a8a4efa26dc9328041d61a9d86125eb10
lethisa <lethisaputri@gmail.com>
2018-07-19 12:49:32 +0700

    ManyToMany Sample

--- ManyToMany/app/Http/routes.php
+++ ManyToMany/app/Http/routes.php
@@ -1,5 +1,8 @@
 <?php
 
+
+Use App\User;
+Use App\Role;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,4 +16,77 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+    
+    $user = User::find(2);
+
+    $role = new Role(['name'=>'admin']);
+
+    $user->roles()->save($role);
+
+});
+
+Route::get('/read', function () {
+    
+    $user = User::findOrFail(1);
+
+    foreach ($user->roles as $role) {
+        return $role;
+    }
+});
+
+Route::get('/update', function () {
+    
+    $user = User::findOrFail(1);
+
+    if ($user->has('roles')) {
+        foreach ($user->roles as $role) {
+            if ($role->name == 'administrator') {
+                $role->name = strtoupper("administrator");
+
+                $role->save();
+            }
+        }
+    }
+
+});
+
+Route::get('/delete', function () {
+    
+    $user = User::findOrFail(2);
+
+    // delete refer pivot
+    // $user->roles()->delete();
+
+    foreach ($user->roles as $role) {
+        $role->whereId(2)->delete();
+    }
+});
+
+// attach role to user
+Route::get('/attach', function () {
+    
+    $user = User::findOrFail(1);
+
+    $user->roles()->attach(2);
+});
+
+// detach role from user
+Route::get('/detach', function () {
+    
+    $user = User::findOrFail(1);
+
+    $user->roles()->detach(2);
+});
+
+// sync
+Route::get('/sync', function () {
+    
+    $user = User::findOrFail(2);
+
+    $user->roles()->sync([3,1]);
+
+    
 });
