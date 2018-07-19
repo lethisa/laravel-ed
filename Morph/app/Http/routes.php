aa13f43da476327666e6df7bf8f4c5f7e17c9ffa
lethisa <lethisaputri@gmail.com>
2018-07-19 17:14:38 +0700

    polymorphic

--- Morph/app/Http/routes.php
+++ Morph/app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Staff;
+use App\Photo;
+use App\Product;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -13,4 +15,59 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+    
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['path'=>'example.jpg']);
+});
+
+Route::get('/read', function () {
+    
+    $staff = Staff::findOrFail(1);
+
+    foreach($staff->photos as $photo)
+    {
+        return $photo->path;
+    }
+
+});
+
+Route::get('/update', function () {
+    
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "update-01.png";
+
+    $photo->save();
+
+});
+
+Route::get('/delete', function () {
+    
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(1)->delete();
+});
+
+Route::get('/assign', function () {
+    $staff = Staff::findOrFail(2);
+
+    $photo = Photo::findOrFail(7);
+
+    $staff->photos()->save($photo);
+
+});
+
+Route::get('/unassign', function () {
+    $staff = Staff::findOrFail(2);
+
+    // $photo = Photo::findOrFail(7);
+
+    $staff->photos()->whereId(7)->update(['imageable_id'=>'','imageable_type'=>'']);
+
 });
